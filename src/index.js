0590c4423c5b692154807519052bb4109c7ad8e2
Stanislav Bychkov <stanislav.bychkov67@gmail.com>
2021-01-07 13:45:57 +0200

    winning combination and checkWin function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ const allCells = document.querySelectorAll('.cell');
 const xTurn = 'ch';
 const circleTurn = 'r';
 let startOfTurn = xTurn;
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// const winningCombinations ={
+//   horizontalWonCombination:[
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+// ],
+//   verticalWonCombination: [
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+// ],
+//  diagonalRightWonCombination: [0, 4, 8],
+//  diagonalLeftWonCombination: [2, 4, 6],
+// };
 
 
-  allCells.forEach(cell => {
+allCells.forEach(cell => {
   cell.addEventListener('click', handleClick)
 })
 
@@ -12,9 +37,9 @@ function handleClick(e) {
   const cell = e.target;
   let currentClass = startOfTurn ? xTurn : circleTurn;
   placeMark(cell, currentClass);
-/*  if (checkWin(currentClass)){
+  if (checkWin(currentClass)) {
     console.log('winner');
-  }*/
+  }
   swapTurns();
 }
 
@@ -25,3 +50,12 @@ function placeMark(cell, currentClass) {
 function swapTurns() {
   startOfTurn = !startOfTurn;
 }
+
+function checkWin(currentClass) {
+  return winningCombinations.some(combination => {
+    return combination.every(index => {
+      return allCells[index].classList.contains(currentClass);
+    })
+  })
+}
+
